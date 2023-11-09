a62229bdf8b66e2cd3e473aec778ab224f417d60
Javier Valdez <al22220282@utcj.edu.mx>
2023-11-08 19:03:32 -0700

    dropdown en nav de admins

--- layouts_admins/navbar.php
+++ layouts_admins/navbar.php
@@ -62,17 +62,17 @@
             <li><a class="dropdown-item" href="incidencias_online.php">En línea</a></li>
           </ul>
         </li>
-        <!-- <li class="nav-item">
-          <a class="nav-link" href="incidencias.php">Incidencias más destacadas</a>
-        </li> -->
-        <li class="nav-item">
-          <a class="nav-link" href="profesores.php">Profesores</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="carreras.php">Carreras</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="directores.php">Directores</a>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Escolares
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="profesores.php">Profesores</a></li>
+            <li><a class="dropdown-item" href="carreras.php">Carreras</a></li>
+            <li><a class="dropdown-item" href="directores.php">Directores</a></li>
+            <li><a class="dropdown-item" href="#">Grupos</a></li>
+            <li><a class="dropdown-item" href="#">Aulas</a></li>
+          </ul>
         </li>
         <li class="nav-item">
           <a class="nav-link nav-register" href="../layouts/registros.php">Registrar admin</a>
